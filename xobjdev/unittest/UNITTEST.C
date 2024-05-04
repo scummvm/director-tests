c659af1f33dce60c5bbb397b885c12259bde5fb4
Scott Percival <code@moral.net.au>
2024-05-04 15:42:35 +0800

    UNITTEST: Add mouse event injection

--- xobjdev/unittest/UNITTEST.C
+++ xobjdev/unittest/UNITTEST.C
@@ -2,6 +2,8 @@
 #include <windows.h>
 #include "XObject.h"
 
+#define DEBUG 0
+
 /*
 ------------------------------------------------------------------------------
 Method Function Prototypes:
@@ -15,9 +17,16 @@ long __far __pascal __export _unittest_mNew(
     LxProcTablePtr xtbl, LxMemHandle hUnitTest);
 long __far __pascal __export _unittest_mDispose(
     LxProcTablePtr xtbl, LxMemHandle hUnitTest); 
+long __far __pascal __export _unittest_mIsRealDirector(
+    LxProcTablePtr xtbl, LxMemHandle hUnitTest);
 long __far __pascal __export _unittest_mScreenshot(
     LxMemHandle hPath, LxProcTablePtr xtbl, LxMemHandle hUnitTest);
-
+long __far __pascal __export _unitTest_mMoveMouse(
+    long x, long y,  LxProcTablePtr xtbl, LxMemHandle hUnitTest);
+long __far __pascal __export _unitTest_mLeftMouseDown(
+    LxProcTablePtr xtbl, LxMemHandle hUnitTest);
+ long __far __pascal __export _unitTest_mLeftMouseUp(
+    LxProcTablePtr xtbl, LxMemHandle hUnitTest);
 
 /*
 ------------------------------------------------------------------------------
@@ -165,6 +174,12 @@ long __far __pascal __export _unittest_mDispose(
     return (lUnitTestError);
 }
 
+long __far __pascal __export _unittest_mIsRealDirector(
+    LxProcTablePtr xtbl, LxMemHandle hUnitTest)
+{
+    return 1;
+}
+
 long __far __pascal __export _unittest_mScreenshot(
     LxMemHandle hPath, LxProcTablePtr xtbl, LxMemHandle hUnitTest)
 {
@@ -199,7 +214,9 @@ long __far __pascal __export _unittest_mScreenshot(
     SetStretchBltMode(hMemDC, COLORONCOLOR);
     iBits = GetDeviceCaps(hWindowDC, BITSPIXEL) * GetDeviceCaps(hWindowDC, PLANES);
     GetSystemPaletteEntries(hWindowDC, 0, 256, &lppColors);
+#if DEBUG
     LogMessage( xtbl, "Creating a bitmap - width: %d, height: %d, bpp: %d\n", width, height, iBits );
+#endif
 
     bmInfo.biSize = sizeof(BITMAPINFOHEADER);
     bmInfo.biWidth = width;
@@ -214,32 +231,41 @@ long __far __pascal __export _unittest_mScreenshot(
     bmInfo.biClrImportant = 0;
     bmInfo.biSizeImage = ((((DWORD)width * (DWORD)bmInfo.biBitCount + 31) & ~31) >> 3) * (DWORD)height;
     hbWindow = CreateCompatibleBitmap(hWindowDC, width, height);
+#if DEBUG
     LogMessage( xtbl, "CreateCompatibleBitmap: %d\n", hbWindow );
     LogMessage( xtbl, "Bitmap image data size: %ld bytes\n", (DWORD)bmInfo.biSizeImage);
-
+#endif
     hgPrev = SelectObject(hMemDC, hbWindow);
     sizeDIB = (DWORD)sizeof(BITMAPINFOHEADER) + (DWORD)bmInfo.biSizeImage;
     if (iBits == 8) {
         sizeDIB += (DWORD)sizeof(lppColors);
     }
+#if DEBUG
     LogMessage( xtbl, "Bitmap data size: %ld bytes\n", sizeDIB);
+#endif
     hDIB = GlobalAlloc(GHND, sizeDIB);
     lpBitmap = (LPBITMAPINFOHEADER)GlobalLock(hDIB);
     *lpBitmap = bmInfo;
     lpBits = (LPSTR)lpBitmap + *(LPDWORD)lpBitmap;
     if (iBits == 8) {
+#if DEBUG
         LogMessage( xtbl, "Writing %d bytes of palette data\n", sizeof(lppColors));
+#endif
         memcpy(lpBits, lppColors, sizeof(lppColors));
         lpBits += sizeof(lppColors);
     }
 
     // copy from the window device context to the memory device context
     response = BitBlt(hMemDC, 0, 0, width, height, hWindowDC, 0, 0, SRCCOPY);
+#if DEBUG
     LogMessage( xtbl, "BitBlt response: %d\n", response );
+#endif
 
     SelectObject(hMemDC, hgPrev);
     response = GetDIBits(hWindowDC, hbWindow, 0, height, lpBits, (LPBITMAPINFO) lpBitmap, DIB_RGB_COLORS);
+#if DEBUG
     LogMessage( xtbl, "GetDIBits response: %ld %d\n", lpBitmap, response );
+#endif
 
     // avoid memory leak
     DeleteObject(hbWindow);
@@ -249,7 +275,9 @@ long __far __pascal __export _unittest_mScreenshot(
     pPath = xtbl->mem_Lock(hPath);
 
     // A file is created, this is where we will save the screen capture.
+#if DEBUG
     LogMessage( xtbl, "Opening file %s\n", pPath );
+#endif
     hFile = _lcreat(pPath, OF_WRITE);
     
     xtbl->mem_Unlock(hPath);
@@ -282,3 +310,36 @@ long __far __pascal __export _unittest_mScreenshot(
     return 0;
 }
 
+long __far __pascal __export _unitTest_mMoveMouse(
+    long x, long y, LxProcTablePtr xtbl, LxMemHandle hUnitTest) {
+    HWND hWnd;
+    POINT ptCursor;
+    hWnd = xtbl->getStageWindow();
+    ptCursor.x = x;
+    ptCursor.y = y;
+    ClientToScreen(hWnd, &ptCursor);
+    SetCursorPos(ptCursor.x, ptCursor.y);
+    return 0;
+};
+
+long __far __pascal __export _unitTest_mLeftMouseDown(
+    LxProcTablePtr xtbl, LxMemHandle hUnitTest) {
+    HWND hWnd;
+    POINT ptCursor;
+    hWnd = xtbl->getStageWindow();
+    GetCursorPos(&ptCursor);
+    ScreenToClient(hWnd, &ptCursor);
+    SendMessage(hWnd, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(ptCursor.x, ptCursor.y));
+    return 0;
+};
+
+long __far __pascal __export _unitTest_mLeftMouseUp(
+    LxProcTablePtr xtbl, LxMemHandle hUnitTest) {
+    HWND hWnd;
+    POINT ptCursor;
+    hWnd = xtbl->getStageWindow();
+    GetCursorPos(&ptCursor);
+    ScreenToClient(hWnd, &ptCursor);
+    SendMessage(hWnd, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(ptCursor.x, ptCursor.y));
+    return 0;
+};
